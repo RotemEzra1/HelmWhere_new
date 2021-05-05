09f120d71c4fbac96d151a56d73c55ce87634968
RotemEzra1 <rotem19961@gmail.com>
2021-05-05 20:43:50 +0300

    new update

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,7 @@ function toggleClassName(el, className) {
   }
 }
 
+
 // Open the side nav on click
 menuIconEl.on('click', function() {
   toggleClassName(sidenavEl, 'active');
@@ -20,4 +21,26 @@ menuIconEl.on('click', function() {
 // Close the side nav on click
 sidenavCloseEl.on('click', function() {
   toggleClassName(sidenavEl, 'active');
+});
+
+
+
+function toggleResetPswd(e){
+  e.preventDefault();
+  $('#logreg-forms .form-signin').toggle() // display:block or none
+  $('#logreg-forms .form-reset').toggle() // display:block or none
+}
+
+function toggleSignUp(e){
+  e.preventDefault();
+  $('#logreg-forms .form-signin').toggle(); // display:block or none
+  $('#logreg-forms .form-signup').toggle(); // display:block or none
+}
+
+$(()=>{
+  // Login Register Form
+  $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+  $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+  $('#logreg-forms #btn-signup').click(toggleSignUp);
+  $('#logreg-forms #cancel_signup').click(toggleSignUp);
 })();
